a9358131fc893badb7ad998e32bd811687c8ec5b
Manny W <manny.wetterqvist@outlook.com>
2022-03-14 00:20:29 +0900

    part 1 exercises

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,51 @@
+const Header = (props) => {
+
+  return (
+    <h1>{props.course}</h1>
+)
+
+}
+const Content = (props) => {
+
+  return (
+
+    <p>{props.content} {props.exercises}</p>
+  )
+}
+
+const Total = (props) => {
+
+  return (
+
+    <p>Number of exercises {props.sum}</p>
+  )
+}
+
+const App = () => {
+
+  const course = 'Half stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+
+
+
+  return (
+    
+    <div>
+      <Header course={course}/>
+      <Content content = {part1} exercises={exercises1} />
+      <Content content = {part2} exercises={exercises2} />
+      <Content content = {part3} exercises={exercises3} />
+      <Total sum ={exercises1 + exercises2 + exercises3} />
+
+
+    </div>
+)
+}
+
+export default App;
